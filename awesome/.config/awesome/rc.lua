3a03116e5750e918fa25c05da058173fea6bb8b3
Paul Sadauskas <psadauskas@gmail.com>
2016-11-22 10:10:20 -0700

    keepass2 windows float

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -275,6 +275,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
+    { rule = { class = "keepass2" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
