664c8fd093e02ad6982bf203b1ee8f40d43b5760
Paul Sadauskas <psadauskas@gmail.com>
2017-10-20 09:35:11 -0600

    Make polybar depend on screens

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -8,5 +8,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 polybar primary &
-polybar secondary &
+
+if xrandr | grep 'DP-0 connected'; then
+  polybar secondary &
+fi
 
