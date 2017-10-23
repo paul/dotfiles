8e4013f6e5bc618be1a5248be60385c491ede6d7
Paul Sadauskas <psadauskas@gmail.com>
2017-10-23 09:22:34 -0600

    Fix config on dual monitors

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,12 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-PRIMARY_MONITOR=$(xrandr | grep -E " connected (primary )?[1-9]+" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
-SECONDARY_MONITOR=$(xrandr | grep -E " connected ^(primary )?[1-9]+" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+PRIMARY_MONITOR=$(xrandr | grep " connected primary" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+SECONDARY_MONITOR=$(xrandr | grep " connected" | grep -v "primary" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
 
-MONITOR=$PRIMARY_MONITOR polybar primary &
+export MONITOR=$PRIMARY_MONITOR
+polybar primary &
 
-if [ -z ${SECONDARY_MONITOR+x} ]; then
-  MONITOR=$SECONDARY_MONITOR polybar secondary &
+if [[ -v SECONDARY_MONITOR ]]; then
+  export MONITOR=$SECONDARY_MONITOR
+  polybar secondary &
 fi
+
+unset MONITOR
 
