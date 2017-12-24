3b8c0b313d3e3d00594fd4808f9ab248bf974a20
Paul Sadauskas <psadauskas@gmail.com>
2017-12-24 12:36:51 -0700

    Tweak i3 config

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -7,15 +7,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 PRIMARY_MONITOR=$(xrandr | grep " connected primary" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
-SECONDARY_MONITOR=$(xrandr | grep " connected" | grep -v "primary" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+# SECONDARY_MONITOR=$(xrandr | grep " connected" | grep -v "primary" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
 
 export MONITOR=$PRIMARY_MONITOR
 polybar primary &
 
-if [[ -v SECONDARY_MONITOR ]]; then
-  export MONITOR=$SECONDARY_MONITOR
-  polybar secondary &
-fi
+# if [ -n "$SECONDARY_MONITOR" ]; then
+#   export MONITOR=$SECONDARY_MONITOR
+#   polybar secondary &
+# fi
 
 unset MONITOR
 
