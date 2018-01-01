a6a69d89f8e95e931e7e1831054a71e7015154f6
Paul Sadauskas <psadauskas@gmail.com>
2018-01-01 12:02:47 -0700

    Add rofi gh script

---
+++ i3/.config/rofi/gh.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+TOKEN=$(git config --global --includes --get github.token | tr -d '\n')
+AUTH="token ${TOKEN}"
+
+if [ "$@" ]
+then
+  if [[ $@ == */* ]]
+  then
+    coproc ( xdg-open https://github.com/$@ & > /dev/null 2>&1 )
+
+  else
+    # Url-encode query string
+    q=$(printf "${@} in:name" | jq -R -r @uri)
+
+    http -vd GET https://api.github.com/search/repositories\?q=${q} "Authorization:${AUTH}" | jq -r '.items | map(.full_name) | .[]'
+  fi
+else
+  http -vd GET https://api.github.com/user/starred "Authorization:${AUTH}" | jq -r 'map(.full_name) | .[]'
+fi
