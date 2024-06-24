8268529e2037225f1b62442dda09da8bdf985981
Paul Sadauskas <psadauskas@gmail.com>
2024-06-24 11:08:26 -0600

    Update oh-my-zsh

--- zsh/.oh-my-zsh/plugins/docker/README.md
+++ zsh/.oh-my-zsh/plugins/docker/README.md
@@ -30,6 +30,15 @@ file**, but be aware of the side effects:
 > zstyle ':completion:*:*:docker-*:*' option-stacking yes
 > ```
 
+### Use old-style completion
+
+If the current completion does not work well for you, you can enable legacy completion instead with the
+following setting. See https://github.com/ohmyzsh/ohmyzsh/issues/11789 for more information.
+
+```zsh
+zstyle ':omz:plugins:docker' legacy-completion yes
+```
+
 ## Aliases
 
 | Alias   | Command                       | Description                                                                              |
@@ -52,13 +61,15 @@ file**, but be aware of the side effects:
 | dnls    | `docker network ls`           | List all networks the engine daemon knows about, including those spanning multiple hosts |
 | dnrm    | `docker network rm`           | Remove one or more networks                                                              |
 | dpo     | `docker container port`       | List port mappings or a specific mapping for the container                               |
+| dps     | `docker ps`                   | List all the running docker containers                                                   |
+| dpsa    | `docker ps -a`                | List all running and stopped containers                                                  |
 | dpu     | `docker pull`                 | Pull an image or a repository from a registry                                            |
 | dr      | `docker container run`        | Create a new container and start it using the specified command                          |
 | drit    | `docker container run -it`    | Create a new container and start it in an interactive shell                              |
 | drm     | `docker container rm`         | Remove the specified container(s)                                                        |
 | drm!    | `docker container rm -f`      | Force the removal of a running container (uses SIGKILL)                                  |
 | dst     | `docker container start`      | Start one or more stopped containers                                                     |
-| drs     | `docker container restart`    | Restart one or more containers
+| drs     | `docker container restart`    | Restart one or more containers                                                           |
 | dsta    | `docker stop $(docker ps -q)` | Stop all running containers                                                              |
 | dstp    | `docker container stop`       | Stop one or more running containers                                                      |
 | dtop    | `docker top`                  | Display the running processes of a container                                             |
--- zsh/.oh-my-zsh/plugins/extract/extract.plugin.zsh
+++ zsh/.oh-my-zsh/plugins/extract/extract.plugin.zsh
@@ -27,12 +27,25 @@ EOF
     fi
 
     local success=0
-    local extract_dir="${1:t:r}"
     local file="$1" full_path="${1:A}"
+    local extract_dir="${1:t:r}"
+
+    # Remove the .tar extension if the file name is .tar.*
+    if [[ $extract_dir =~ '\.tar$' ]]; then
+      extract_dir="${extract_dir:r}"
+    fi
+
+    # If there's a file or directory with the same name as the archive
+    # add a random string to the end of the extract directory
+    if [[ -e "$extract_dir" ]]; then
+      local rnd="${(L)"${$(( [##36]$RANDOM*$RANDOM ))}":1:5}"
+      extract_dir="${extract_dir}-${rnd}"
+    fi
 
     # Create an extraction directory based on the file name
     command mkdir -p "$extract_dir"
     builtin cd -q "$extract_dir"
+    echo "extract: extracting to $extract_dir" >&2
 
     case "${file:l}" in
       (*.tar.gz|*.tgz)
@@ -56,8 +69,8 @@ EOF
       (*.tar.lz) (( $+commands[lzip] )) && tar xvf "$full_path" ;;
       (*.tar.lz4) lz4 -c -d "$full_path" | tar xvf - ;;
       (*.tar.lrz) (( $+commands[lrzuntar] )) && lrzuntar "$full_path" ;;
-      (*.gz) (( $+commands[pigz] )) && pigz -dk "$full_path" || gunzip -k "$full_path" ;;
-      (*.bz2) bunzip2 "$full_path" ;;
+      (*.gz) (( $+commands[pigz] )) && pigz -cdk "$full_path" > "${file:t:r}" || gunzip -ck "$full_path" > "${file:t:r}" ;;
+      (*.bz2) (( $+commands[pbzip2] )) && pbzip2 -d "$full_path" || bunzip2 "$full_path" ;;
       (*.xz) unxz "$full_path" ;;
       (*.lrz) (( $+commands[lrunzip] )) && lrunzip "$full_path" ;;
       (*.lz4) lz4 -d "$full_path" ;;
@@ -67,17 +80,18 @@ EOF
       (*.rar) unrar x -ad "$full_path" ;;
       (*.rpm)
         rpm2cpio "$full_path" | cpio --quiet -id ;;
-      (*.7z) 7za x "$full_path" ;;
+      (*.7z | *.7z.[0-9]*) 7za x "$full_path" ;;
       (*.deb)
         command mkdir -p "control" "data"
         ar vx "$full_path" > /dev/null
         builtin cd -q control; extract ../control.tar.*
         builtin cd -q ../data; extract ../data.tar.*
         builtin cd -q ..; command rm *.tar.* debian-binary ;;
-      (*.zst) unzstd "$full_path" ;;
-      (*.cab) cabextract "$full_path" ;;
+      (*.zst) unzstd --stdout "$full_path" > "${file:t:r}" ;;
+      (*.cab|*.exe) cabextract "$full_path" ;;
       (*.cpio|*.obscpio) cpio -idmvF "$full_path" ;;
       (*.zpaq) zpaq x "$full_path" ;;
+      (*.zlib) zlib-flate -uncompress < "$full_path" > "${file:r}" ;;
       (*)
         echo "extract: '$file' cannot be extracted" >&2
         success=1 ;;
@@ -97,21 +111,23 @@ EOF
     # - Y2: at most give 2 files
     local -a content
     content=("${extract_dir}"/*(DNY2))
-    if [[ ${#content} -eq 1 && -d "${content[1]}" ]]; then
-      # The extracted folder (${content[1]}) may have the same name as $extract_dir
+    if [[ ${#content} -eq 1 && -e "${content[1]}" ]]; then
+      # The extracted file/folder (${content[1]}) may have the same name as $extract_dir
       # If so, we need to rename it to avoid conflicts in a 3-step process
       #
-      # 1. Move and rename the extracted folder to a temporary random name
+      # 1. Move and rename the extracted file/folder to a temporary random name
       # 2. Delete the empty folder
-      # 3. Rename the extracted folder to the original name
+      # 3. Rename the extracted file/folder to the original name
       if [[ "${content[1]:t}" == "$extract_dir" ]]; then
         # =(:) gives /tmp/zsh<random>, with :t it gives zsh<random>
-        local tmp_dir==(:); tmp_dir="${tmp_dir:t}"
-        command mv -f "${content[1]}" "$tmp_dir" \
+        local tmp_name==(:); tmp_name="${tmp_name:t}"
+        command mv "${content[1]}" "$tmp_name" \
         && command rmdir "$extract_dir" \
-        && command mv -f "$tmp_dir" "$extract_dir"
-      else
-        command mv -f "${content[1]}" . \
+        && command mv "$tmp_name" "$extract_dir"
+      # Otherwise, if the extracted folder name already exists in the current
+      # directory (because of a previous file / folder), keep the extract_dir
+      elif [[ ! -e "${content[1]:t}" ]]; then
+        command mv "${content[1]}" . \
         && command rmdir "$extract_dir"
       fi
     elif [[ ${#content} -eq 0 ]]; then
