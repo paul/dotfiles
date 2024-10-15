4f7f8c4495010a03d7a54c0520f85b1850d2ba9c
Paul Sadauskas <psadauskas@gmail.com>
2024-10-15 12:52:27 -0600

    Update codespaces script

--- setup-codespaces.sh
+++ setup-codespaces.sh
@@ -6,7 +6,9 @@ if [ -z "$USER" ]; then
   USER=$(id -un)
 fi
 
-mv /workspaces/.codespaces/.persistedshare/dotfiles $HOME/dotfiles
+if [ ! -d $HOME/dotfiles ]; then
+  mv /workspaces/.codespaces/.persistedshare/dotfiles $HOME/dotfiles
+fi
 
 cd $HOME
 
@@ -16,8 +18,15 @@ export SUDO_ASKPASS=/bin/true
 # Change shell to zsh
 sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 
-add-apt-repository ppa:neovim-ppa/stable
-apt-get update
-apt-get install neovim
+# Install neovim
+sudo apt-get install -y libfuse2
+# Ubuntu is the worst
+curl -L -o nvim.appimage https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+install -D nvim.appimage $HOME/bin/nvim
+
+cd dotfiles
 
 stow nvim ruby zsh
+
+# Setup Lazyvim
+$HOME/bin/nvim --headless -c 'luafile install.lua' -c 'qall'
