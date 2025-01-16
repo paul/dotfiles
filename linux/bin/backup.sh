a0466ef42b9cc9f31426b3f23925580e17dc776f
Paul Sadauskas <psadauskas@gmail.com>
2025-01-16 10:15:59 -0700

    Add more excludes to backup

--- linux/bin/backup.sh
+++ linux/bin/backup.sh
@@ -3,13 +3,14 @@
 set -euo pipefail
 set -vx
 
-REPO=9669@usw-s009.rsync.net:backups/borg-repo
+BORG_REPO=de2815@de2815.rsync.net:backups/borg-repo
 MACHINE=$(hostname -s)
 export BORG_PASSPHRASE=$(pass show home/rsync.net-borg)
 
 borg create -v --stats                      \
   --compression lz4                         \
-  $REPO::${MACHINE}-'{now:%Y-%m-%d}'        \
+  --one-file-system                         \
+  $BORG_REPO::${MACHINE}-'{now:%Y-%m-%d}'   \
   /home/rando                               \
   --exclude '/home/*/*.log'                 \
   --exclude '/home/*/*.log.*'               \
@@ -18,11 +19,13 @@ borg create -v --stats                      \
   --exclude '/home/rando/Dropbox'           \
   --exclude '/home/rando/NextCloud'         \
   --exclude '/home/rando/.cache'            \
+  --exclude '/home/rando/.var'              \
   --exclude '/home/rando/Downloads'         \
   --exclude '/home/rando/*/Cache/*'         \
   --exclude '/home/rando/.local/share/akonadi/*' \
-  --exclude '/home/rando/.local/share/containers'
+  --exclude '/home/rando/.local/share/containers' \
+  --exclude '/home/rando/.local/share/baloo' 
 
-borg prune -v --list --stats $REPO --prefix ${MACHINE}- --save-space \
+borg prune -v --list --stats $BORG_REPO --prefix ${MACHINE}- --save-space \
   --keep-daily=7 --keep-weekly=4 --keep-monthly=12
 
